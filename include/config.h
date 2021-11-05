958feffb87ca3c07cee24c16c5b94dcefd0c7676
puskapeti <puskapeti12@gmail.com>
2021-11-05 09:16:41 +0100

    added interrupt handling and io debouncing

--- include/config.h
+++ include/config.h
@@ -7,18 +7,44 @@
 
 #include <Arduino.h>
 
-#define MAX_EXTRUDE_SPEED   100
+#define DEBUG
+#define SERIAL_CONTROL
+#define IO_DEBOUNCE
 
-#define EXTRUDE_PIN         43  //PD0 INT0
-#define RETRACT_PIN         45  //PD1 INT2
-#define SPEED_SCALE_PIN_0   0
-#define SPEED_SCALE_PIN_1   0
-#define SPEED_SCALE_PIN_2   0
-#define SPEED_SCALE_PIN_3   0
+#define MAX_EXTRUDE_SPEED   1000.0
+#define MOTOR_ACCELERATION  1000.0
+
+#define EXTRUDE_PIN         2  //PE4 INT4
+#define RETRACT_PIN         3  //PE3 INT5
+#define SPEED_SCALE_PIN_0   16
+#define SPEED_SCALE_PIN_1   17
+#define SPEED_SCALE_PIN_2   23
+#define SPEED_SCALE_PIN_3   25
 
 // E0
-#define STEPPER_DIR_PIN     28
+/*#define STEPPER_DIR_PIN     28
 #define STEPPER_EN_PIN      24
-#define STEPPER_PULSE_PIN   26
+#define STEPPER_PULSE_PIN   26*/
+
+// E1
+#define STEPPER_DIR_PIN     34
+#define STEPPER_EN_PIN      30
+#define STEPPER_PULSE_PIN   36
+
+#define STEPS_PER_REV   200.0
+#define MICROSTEPS      16.0
+
+#define DEG2STEPS(deg)  ((float) deg / 360.0 * STEPS_PER_REV * MICROSTEPS)
+
+#ifdef DEBUG
+#define DPRINT(msg) Serial.print(msg)
+#else
+#define DPRINT(msg)
+#endif
+
+//debouncing
+#ifdef IO_DEBOUNCE
+#define DEBOUNCE_TIME 50 //ms
+#endif
 
 #endif //ROBOTEXTRUDER_CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,58 @@
 
 #include "config.h"
 
-AccelStepper motor = AccelStepper(AccelStepper::DRIVER, STEPPER_PULSE_PIN, STEPPER_EN_PIN);
+void pinSetup();
+void ISRSetup();
+
+void stepForward();
+void stepBackward();
+void enableMotor();
+void disableMotor();
+
+void extruderInterruptCallback();
+void retractInterruptCallback();
+
+AccelStepper motor = AccelStepper(stepForward, stepBackward);
 boolean motorIsRunning = false;
 boolean motorIsStopping = false;
+
+void setup() {
+#ifdef DEBUG
+    Serial.begin(115200);
+#endif
+    pinSetup();
+    ISRSetup();
+
+    motor.setMaxSpeed(MAX_EXTRUDE_SPEED);
+    motor.setAcceleration(MOTOR_ACCELERATION);
+    enableMotor();
+    motor.move(DEG2STEPS(360));
+}
+
+void loop() {
+// write your code here
+    long stepsToStop = (long)((MAX_EXTRUDE_SPEED * MAX_EXTRUDE_SPEED) / (2.0 * MOTOR_ACCELERATION));
+    if (motorIsRunning && !motorIsStopping) {
+        motor.run();
+        if (motor.distanceToGo() < DEG2STEPS(stepsToStop) * 2) {
+            motor.move(DEG2STEPS(360));
+        }
+    }
+    if (motorIsStopping) {
+        motorIsRunning = false;
+        motor.stop();
+        motor.runToPosition();
+        motorIsStopping = false;
+    }
+}
 
 void pinSetup() {
     pinMode(STEPPER_PULSE_PIN, OUTPUT);
     pinMode(STEPPER_EN_PIN, OUTPUT);
     pinMode(STEPPER_DIR_PIN, OUTPUT);
 
-    pinMode(EXTRUDE_PIN, INPUT);
-    pinMode(RETRACT_PIN, INPUT);
+    pinMode(EXTRUDE_PIN, INPUT_PULLUP);
+    pinMode(RETRACT_PIN, INPUT_PULLUP);
 
     pinMode(SPEED_SCALE_PIN_0, INPUT);
     pinMode(SPEED_SCALE_PIN_1, INPUT);
@@ -22,46 +63,100 @@ void pinSetup() {
 }
 
 void ISRSetup() {
-    //control register
-    EICRA = 0;
-    EICRA |= (1 << ISC00) | (1 << ISC10); // falling edge detection
-
-    //mask register
-    EIMSK = 0;
-    EIMSK |= (1 << INT0) | (1 << INT2);
+    attachInterrupt(digitalPinToInterrupt(EXTRUDE_PIN), extruderInterruptCallback, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(RETRACT_PIN), retractInterruptCallback, RISING);
 }
 
-void setup() {
-    pinSetup();
-    ISRSetup();
-
-    motor.setEnablePin(STEPPER_EN_PIN);
-    motor.setSpeed(MAX_EXTRUDE_SPEED);
+void stepForward() {
+    digitalWrite(STEPPER_DIR_PIN, HIGH);
+    digitalWrite(STEPPER_PULSE_PIN, HIGH);
+    digitalWrite(STEPPER_PULSE_PIN, LOW);
 }
 
-void loop() {
-// write your code here
-    if (motorIsRunning) {
-        motor.runSpeed();
+void stepBackward() {
+    digitalWrite(STEPPER_DIR_PIN, LOW);
+    digitalWrite(STEPPER_PULSE_PIN, HIGH);
+    digitalWrite(STEPPER_PULSE_PIN, LOW);
+}
+
+void enableMotor() {
+    digitalWrite(STEPPER_EN_PIN, LOW);
+}
+
+void disableMotor() {
+    digitalWrite(STEPPER_PULSE_PIN, HIGH);
+}
+
+#ifdef SERIAL_CONTROL
+void serialEvent() {
+    String msg = Serial.readString();
+    if (msg == "extrude\n") {
+        if (!motorIsRunning) {
+            motorIsRunning = true;
+            Serial.println("Motor running");
+        }
+        else {
+            motorIsStopping = true;
+            Serial.println("Motor stopping");
+        }
     }
-    if (motorIsStopping) {
-        motor.stop();
-
-        motorIsRunning = false;
-        motorIsStopping = false;
-    }
 }
+#endif
 
-
+unsigned long eISRLastCalled = 0;
 
 /* INTERRUPTS */
+void extruderInterruptCallback() {
+#ifdef IO_DEBOUNCE
+    unsigned long eISRCalled = millis();
+    if (eISRCalled - eISRLastCalled < DEBOUNCE_TIME) {
+        //bouncing
+        return;
+    }
+    else {
+        //stopped bouncing
+        eISRLastCalled = eISRCalled;
+    }
+#endif
+    Serial.println("extruder interrupt");
+    if (digitalRead(EXTRUDE_PIN) == HIGH) { // rising edge
+/*        byte speedScale = 0b00000001;
+        if (digitalRead(SPEED_SCALE_PIN_0) == HIGH) {
+            speedScale |= (1 << 0);
+        }
+        if (digitalRead(SPEED_SCALE_PIN_1) == HIGH) {
+            speedScale |= (1 << 1);
+        }
+        if (digitalRead(SPEED_SCALE_PIN_2) == HIGH) {
+            speedScale |= (1 << 2);
+        }
+        if (digitalRead(SPEED_SCALE_PIN_3) == HIGH) {
+            speedScale |= (1 << 3);
+        }
+        int speedScaleInt = (int) speedScale;
+        float speed = MAX_EXTRUDE_SPEED / (float) speedScaleInt;
+        motor.setSpeed(speed);
+        DPRINT(speed);
+        DPRINT(speedScaleInt);*/
 
-ISR (INT0_vect) {
-    printf("Interrupt detected");
-    if (digitalRead(EXTRUDE_PIN) == HIGH) { // LOW -> HIGH
         motorIsRunning = true;
     }
-    else { // HIGH -> LOW
+    else { // falling edge
         motorIsStopping = true;
     }
+}
+
+void retractInterruptCallback() {
+#ifdef IO_DEBOUNCE
+    unsigned long eISRCalled = millis();
+    if (eISRCalled - eISRLastCalled < DEBOUNCE_TIME) {
+        //bouncing
+        return;
+    }
+    else {
+        //stopped bouncing
+        eISRLastCalled = eISRCalled;
+    }
+#endif
+    Serial.println("Retract callback");
 }
